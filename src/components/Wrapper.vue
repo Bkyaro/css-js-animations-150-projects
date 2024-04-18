1ca3bce4411b4adc1499a16e611e509a43e9cc65
Jackal_lzt <bkyarokun@gmail.com>
2024-04-18 16:23:04 +0800

    feat: add routeback opacity transition on component

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="wrapper">
     <component
+      class="component-item"
+      :class="{ 'component-item-fade': isLeaving }"
       :is="this.target ? this.target : this.lists[this.$route.query.name]"
       :coord="this.coord"
       :isHome="this.isHome"
@@ -20,12 +22,24 @@ export default {
         : null,
       lists: Lists,
       isHome: this.$router.history.current.fullPath === "/",
+      isLeaving: false,
     };
+  },
+  beforeRouteLeave(to, from, next) {
+    this.isLeaving = true;
+    setTimeout(() => {
+      next();
+    }, 250);
   },
 };
 </script>
 <style scoped>
-.wrapper {
-  /* border: 2px solid black; */
+.component-item {
+  opacity: 1;
+  transition: opacity 250ms ease-in-out;
+}
+
+.component-item-fade {
+  opacity: 0;
 }
 </style>
